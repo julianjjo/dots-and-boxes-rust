0910e7755cde5726140ee15a1ff06e0fe9055489
Julian Mican <julian.mican@gmail.com>
2024-04-23 20:52:29 -0500

    Fix clippy errors

--- src/util.rs
+++ src/util.rs
@@ -3,7 +3,7 @@ use std::collections::HashSet;
 pub fn calulate_score(grid: Vec<Vec<Vec<Vec<i8>>>>, player_1: &mut i32, player_2: &mut i32) {
     for row in grid.iter() {
         for column in row.iter() {
-            let amount_player_lines = count_unique_numbers(&column[1]);
+            let amount_player_lines = count_unique_numbers(column[1].clone());
             if amount_player_lines == 1 && column[1][0] != 0 {
                 if column[1][0] == 1 {
                     *player_1 += 1;
@@ -19,9 +19,9 @@ pub fn is_valid_position<T>(index: usize, vector: Vec<T>) -> bool {
     index < vector.len()
 }
 
-pub fn count_unique_numbers(vector: &Vec<i8>) -> usize {
+pub fn count_unique_numbers(vector: Vec<i8>) -> usize {
     let mut unique_set = HashSet::new();
-    for &num in vector {
+    for num in vector {
         unique_set.insert(num);
     }
     unique_set.len()
@@ -34,18 +34,18 @@ mod tests {
     #[test]
     fn count_unique_numbers_test() {
         let vector: Vec<i8> = vec![1, 1, 1, 1];
-        assert_eq!(count_unique_numbers(&vector), 1);
+        assert_eq!(count_unique_numbers(vector.clone()), 1);
     }
 
     #[test]
     fn count_unique_numbers_two_test() {
         let vector: Vec<i8> = vec![1, 1, 0, 0];
-        assert_eq!(count_unique_numbers(&vector), 2);
+        assert_eq!(count_unique_numbers(vector.clone()), 2);
     }
 
     #[test]
     fn is_valid_position_test() {
         let vector: Vec<i8> = vec![1, 1, 1, 1];
-        assert_eq!(is_valid_position(1, &vector), true);
+        assert_eq!(is_valid_position(1, vector.clone()), true);
     }
 }
